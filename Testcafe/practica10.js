60540a745b2606dfbe1bd56ce43f874ce56f9e2f
Fernando Espinoza <fespinoza@giniem.com>
2020-10-24 12:29:01 -0600

    fix

--- Testcafe/practica10.js
+++ Testcafe/practica10.js
@@ -1,38 +1,27 @@
+
 import page from './pageModel';
 
-fixture('Test con testcade')
-    .page('https://the-internet.herokuapp.com/')
 
-<<<<<<< HEAD
-test('Probar un clinck derecho en una ventana js alert', async t => {
-=======
-test('Probar un clinck derecho', async t => {
->>>>>>> master
+fixture('Test con testcafe')
+    .page ('https://the-internet.herokuapp.com/');
 
+
+test('Probar un click derecho', async t => {
     await t
         .click(page.link10)
-
+    
     await t
-        
-<<<<<<< HEAD
+
         .setNativeDialogHandler(() => true)
         .rightClick(page.hotspot101)
 
         const history = await t.getNativeDialogHistory();
 
         console.log(history)
-    
-    await t
 
+    await t
         .wait(200)
         .expect(history[0].type).eql('alert')
         .expect(history[0].text).eql('You selected a context menu')
         .expect(history[0].url).eql('https://the-internet.herokuapp.com/context_menu')
-=======
-        .setNativeDialogHandler()
-        .rightClick(page.hostpot101)
->>>>>>> master
-
-
-
-});
+    });
--- Testcafe/practica9.js
+++ Testcafe/practica9.js
@@ -1,54 +1,47 @@
 import page from './pageModel';
 
-fixture('Test con testcade')
-    .page('https://the-internet.herokuapp.com/')
+fixture('Test con testcafe')
+    .page('https://the-internet.herokuapp.com/');
 
-test('Probar presionar teclas', async t => {
-
+test('Probar presionar teclas', async t =>{
     await t
         .click(page.link9)
+    
+    await t
+        .pressKey('backspace')
+        .wait(500)
+    
+    await t
+        .pressKey('c')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: C')
+        .expect(page.text91.innerText).notContains('53$%&/$%&$%&$%')
 
-        await t
-            .pressKey('backspace')
-            .wait(500)
+        .pressKey('tab')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: TAB')
 
-        await t
-            .pressKey('c')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: C')
-            .expect(page.text91.innerText).notContains('You entered: backspace')
+        .pressKey('ENTER')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: ENTER')
 
-           
-            .pressKey('tab')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: TAB')
+        .pressKey('esc')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: ESC')
 
-            .pressKey('ENTER')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: ENTER')
+        .pressKey('capslock')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: CAPS_LOCK')
 
-            .pressKey('esc')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: ESC')
+        .pressKey('space')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: SPACE')
 
-            .pressKey('capslock')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: CAPS_LOCK')
-            
-            .pressKey('space')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: SPACE')
+        .pressKey('shift')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: SHIFT')
 
-            .pressKey('shift')
-            .wait(500)
-            .expect(page.text91.innerText).contains('You entered: SHIFT')
-
-            .pressKey('up')
-            .wait(500)
-
-
-<<<<<<< HEAD
-     });
-=======
-     });
->>>>>>> master
+        .pressKey('up')
+        .wait(500)
+        .expect(page.text91.innerText).contains('You entered: UP')
+});
