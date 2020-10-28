6ebadeae9e46b5a33c59d3ade65b3038170e37ec
Fernando Espinoza <fespinoza@giniem.com>
2020-10-27 21:29:18 -0600

    practica del 27oct

---
+++ E-comerce/data.js
@@ -0,0 +1,12 @@
+let rndNumber = Math.random().toString(36).substr(1,4);
+
+let data = { 
+
+ email:'fer95' + rndNumber + '@mailinator.com',
+ firstName: "Fernando",
+ lastName: "Espinoza",
+ password:'888225',
+ pBox:'P.O. BOX 5056',
+ city: 'Redmon'
+}
+export{data}
--- E-comerce/mi_Cuenta.js
+++ E-comerce/mi_Cuenta.js
@@ -1,39 +1,50 @@
 import page from './PageObjectModel';
+import { data } from  './data'
 
+/*
 let rndNumber = Math.random().toString(36).substr(1,4);
 const email = 'fer95' + rndNumber + '@mailinator.com'
 const firstName = "Fernando"
 const lastName = "Espinoza"
 const password = '888225'
+*/
 
 
 fixture('Preuba de modulo de cuenta')
     .page('http://automationpractice.com/');
 
-    test('Crear una cuneta', async t => {
+    test('Crear una cunenta', async t => {
 
         await t
             .click(page.signIn_link)
 
-            console.log("Correo: ", email)
+            console.log("Correo: ", data.email)
+            console.log("firstName: ", data.firstName)
+            console.log("LastName: ", data.lastName)
         
         await t
-            .typeText(page.email_input,email)
+            .typeText(page.email_input,data.email)
             .click(page.createAccout_btn)
 
         await t
-            .expect(page.email_form.value).contains(email)
+            .expect(page.email_form.value).contains(data.email)
             .expect(page.email_form.hasAttribute('readonly')).notOk()
             //
 
-        .typeText(page.firsname_input, firstName)
-        .typeText(page.lastname_input, lastName)
+        .typeText(page.firsname_input, data.firstName)
+        .typeText(page.lastname_input, data.lastName)
 
-        .expect(page.firstname_Address.value).contains(firstName)
-        .expect(page.lastname_Address.value).contains(lastName)
+        .expect(page.firstname_Address.value).contains(data.firstName)
+        .expect(page.lastname_Address.value).contains(data.lastName)
 
-        typeText(page.password_input, password)
-        .typeText(page.address, ' P.O. BOX 5056')
+        .typeText(page.password_input, data.password)
+        .typeText(page.address, data.pBox)
+        .expect(page.address.value).contains(data.pBox)
+        
+        .typeText(page.city, data.city)
+        .expect(page.city).contains(data.city)
+
+        typeText(page.address,data.pBox)
 
 
 
